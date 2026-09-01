7f0a97f55b04768d23dc2318e33009f3de92129a
Rosmery_funes <zuleymafunes07@gmail.com>
2026-09-01 17:27:51 -0600

    Delete ejercicio7.js

--- ejercicio7.js
+++
@@ -1,8 +0,0 @@
-let fecha = new Date()
-let hora = fecha.getHours()
-let minutos = fecha.getMinutes();
-let segundos = fecha.getSeconds()
-
-console.log(`la hora es ${hora}:${minutos}:${segundos}`)
-
-
