73a5ceffe626d1d23f78436c399280a88d49bf22
Rosmery_funes <zuleymafunes07@gmail.com>
2026-09-01 17:27:39 -0600

    Delete ejercicio6.js

--- ejercicio6.js
+++
@@ -1,9 +0,0 @@
-let nombre = 'zule'
-let apellido = 'funes'
-let anio = 2007;
-let promedio = 9.1111;
-
-console.log(`su nombre y apellido en mayusulas es ${nombre.toUpperCase()} y ${apellido.toUpperCase()}`)
-console.log(`las primeras 3 letras del apellido son ${apellido.slice(0,3)}`)
-console.log(`los ultimos dos digitos del anio son ${anio.toString().slice(2,4)}`)
-console.log(`su promedio con dos decmales es ${promedio.toFixed(2)}`)
