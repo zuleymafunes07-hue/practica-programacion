a296be7933446c99945257269be4cbf67d96832f
Rosmery_funes <zuleymafunes07@gmail.com>
2026-09-01 17:27:05 -0600

    Delete ejercicio3.js

--- ejercicio3.js
+++
@@ -1,7 +0,0 @@
-let numero = 12345678;
-
-let numerotexto = numero.toString()
-
-console.log(`su variable es de tipo: ${typeof numerotexto}`);
-
-console.log(`los primeros cuatro digitos son ${numerotexto.slice(0,4)}`);
