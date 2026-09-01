8bf9a1608a445c2b24520c7671554eac8f3ed17c
Rosmery_funes <zuleymafunes07@gmail.com>
2026-09-01 17:28:02 -0600

    Delete tarea1.js

--- tarea1.js
+++
@@ -1,9 +0,0 @@
-let nombreCompleto = 'Carlos Eduardo Martínez';
-
-let nombreMayus = nombreCompleto.toUpperCase();
-let primeras4Letras = nombreCompleto.slice(0, 4);
-let nombreMinus = nombreCompleto.toLowerCase();
-
-console.log(`El nombre en mayúsculas es: ${nombreMayus}`);
-console.log(`Las primeras 4 letras del nombre son: ${primeras4Letras}`);
-console.log(`El nombre en minúsculas es: ${nombreMinus}`);
